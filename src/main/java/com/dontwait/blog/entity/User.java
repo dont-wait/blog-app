de6c5ebea2630a6968693f6385e5bc828364374b
dont-wait <nguyentansang15042005@gmail.com>
2025-02-27 22:23:47 +0700

    fix lombok plugin config

--- src/main/java/com/dontwait/blog/entity/User.java
+++ src/main/java/com/dontwait/blog/entity/User.java
@@ -1,6 +1,7 @@
 package com.dontwait.blog.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.Setter;
 @Entity
 @Table(name="users")
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class User {
